6fee19e1e5bc7b97f2c1f37ef6ad6cae00a0ea08
henmohr <henriquemohr@redes.ufsm.br>
2026-08-11 14:34:12 -0300

    chore: promover integração do Strapi para produção

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default ({ env }: { env: any }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('CMS_JWT_SECRET', env('JWT_SECRET')),
+    },
+  },
+});
--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,7 +1,9 @@
 export default ({ env }: { env: any }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  proxy: env.bool('IS_PROXIED', true),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('CMS_APP_KEYS', env.array('APP_KEYS')),
   },
 });
